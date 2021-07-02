d1a7eafe3363edfea437da510b2c8eaaf1ae9a44
arturs sprogis <arturs.sprogis@gmail.com>
2021-07-02 21:59:25 +0300

    Updated logo API service

--- Dockerfile
+++ Dockerfile
@@ -1,38 +1,9 @@
-#FROM python:3-alpine
-#FROM bitnami/pytorch
-
-#FROM khammami/detectron2
-
 FROM ylashin/detectron2
-
-
-#RUN apt-get update
-#RUN apt-get install -y git
-
-
-#RUN pip install Flask-PyMongo
-#RUN pip install PyJWT
-#RUN pip install flask_uploads
-#RUN pip install flask-login
-
-#RUN pip install opencv-python-headless
-
-#RUN apt-get update -y
-#RUN apt-get install -y python-pip python-dev build-essential python-opencv
 
 RUN pip install flask
 RUN pip install requests
-
 RUN pip install torchvision
-
 RUN pip install Pillow
-
-#RUN pip install opencv-python-headless
-
-#RUN pip install -U 'git+https://github.com/facebookresearch/fvcore'
-#RUN git clone https://github.com/facebookresearch/detectron2 detectron2_repo
-#RUN pip install -e detectron2_repo
-
 
 COPY . /app
 WORKDIR /app
@@ -40,14 +11,5 @@ WORKDIR /app
 # Expose the Flask port
 EXPOSE 5000
 
-#CMD [ "python", "-u", "./runaaaa.py" ]
-
-#ENTRYPOINT [ "python" ]
-#CMD [ "-u", "run.py", ]
-
-#CMD [ "python", "-u", "run.py" ]
-
 ENTRYPOINT ["python"]
 CMD ["run.py"]
-
-#CMD [ "python", "-u", "run.py" ]
--- run.py
+++ run.py
@@ -1,12 +1,13 @@
-import os, io
+import os, io, uuid
 import requests
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template, send_file
 
 from detectron2.config import get_cfg
 from detectron2 import model_zoo
 from detectron2.engine import DefaultPredictor
 
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
+
 import numpy as np
 import cv2
 
@@ -18,51 +19,109 @@ CLASS_NAMES = ['Adidas', 'Apple', 'BMW', 'Citroen', 'Cocacola', 'DHL', 'Fedex', 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'some-secret-for-logo-recognition'
+app.model = None
+
+
+
+# @app.route("/")
+# def check():
+#     return jsonify({'Message': "Server Running!"})
 
 
 @app.route("/")
-def check():
-    return jsonify({'Message': "Server Running!"})
+def upload_photo():
+    return render_template('upload.html')
+
+
+@app.route("/init")
+def init_model():
+    init_model()
+    return "Init done"
+
+
+@app.route("/file/<file_name>")
+def display_file(file_name):
+    print ("in get file")
+
+
+    file_path = get_file_path(file_name)
+    return send_file(file_path)
 
 
 @app.route("/predict", methods=['POST'])
 def precict_logo():
+    result = []
 
-    try:
-        file = request.files['image']
+    files = request.files.getlist("images")
+    for file in files:
+
         pil_img = Image.open(file)
-
         numpy_image = np.array(pil_img)
 
         # convert to a openCV2 image, notice the COLOR_RGB2BGR which means that
         # the color is converted from RGB to BGR format
         img = cv2.cvtColor(numpy_image, cv2.COLOR_RGB2BGR)
-        predictor = load_flickr27_model()
+        predictor = get_app_model()
 
         outputs = predictor(img)
         outputs = outputs['instances'].get_fields()
 
-        # pred_boxes = outputs['pred_boxes'].tensor.to(device).numpy()
-        pred_boxes = outputs['pred_boxes']
-        # scores = outputs['scores'].to(device).numpy()
-        scores = outputs['scores']
-        # pred_classes = outputs['pred_classes'].to(device).numpy()
+        pred_boxes = outputs['pred_boxes'].tensor.numpy()
+        scores = outputs['scores'].numpy()
         pred_classes = outputs['pred_classes']
 
         class_names = []
         for cls in pred_classes:
             class_names.append(CLASS_NAMES[int(cls)])
 
-        return jsonify({
-            'num_detections' : len(pred_boxes),
-            'pred_boxes' : str(pred_boxes),
-            'scores' : str(scores),
-            'class_names':class_names,
-        })
+        source_img = Image.open(file).convert("RGBA")
+        draw = ImageDraw.Draw(source_img)
 
-    except:
-        return jsonify({'Message': "API Failed"})
+        for i in range(0, len(pred_boxes)):
+            shape = pred_boxes[i]
+            score = scores[i]
+            class_name = class_names[i]
 
+            draw.rectangle(shape, outline="white", width=2)
+
+            text = class_name + " " + str(round(score, 2))
+            draw.text((shape[0], shape[3] + 10), text)
+
+        img_name = str(uuid.uuid4()) + ".jpg"
+        img_full_name = get_file_path(img_name)
+
+        source_img = source_img.convert('RGB')
+        source_img.save(img_full_name)
+
+
+        result.append({
+                        'file': "/file/"+ img_name,
+                        'num_detections': len(pred_boxes),
+                        'pred_boxes': str(pred_boxes),
+                        'scores': str(scores),
+                        'class_names': class_names,
+                    })
+
+
+        print ("result ", result)
+
+
+    return render_template('result.html', result=result)
+
+
+def get_app_model():
+    return init_model()
+
+
+def init_model():
+    if (app.model == None):
+        print ("In init model")
+        app.model = load_flickr27_model()
+
+    else:
+        print ("Model initialized")
+
+    return app.model
 
 
 def load_flickr27_model(device="cpu"):
@@ -82,8 +141,8 @@ def load_flickr27_model(device="cpu"):
     return predictor
 
 
-
-# predictor = load_flickr27_model('cpu')
+def get_file_path(file_name):
+    return os.path.join("./uploads", file_name)
 
 
 if __name__ == '__main__':
